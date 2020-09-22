2c46e3f4109b91d2e491bb07eb4d2b32dfd32b28
hyzen <hail4heisenberg@gmail.com>
2020-09-23 04:34:59 +0530

    Initial commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAF_h3wFQBIjoKflgpOfEslC-nQe08M1Zg",
+  authDomain: "mygitapp-a96bd.firebaseapp.com",
+  databaseURL: "https://mygitapp-a96bd.firebaseio.com",
+  projectId: "mygitapp-a96bd",
+  storageBucket: "mygitapp-a96bd.appspot.com",
+  messagingSenderId: "745629914868",
+  appId: "1:745629914868:web:8330e8b9735276b9f770a1",
+  measurementId: "G-V7F057BNR1",
+};
---
+++ src/layout/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+const Footer = () => {
+  return (
+    <Container
+      fluid
+      tag="footer"
+      className="text-center bg-info text-white text-uppercase fixed-bottom p-3"
+    >
+      Hyzen Github search app with firebase bitches!!!!!!
+    </Container>
+  );
+};
+
+export default Footer;
